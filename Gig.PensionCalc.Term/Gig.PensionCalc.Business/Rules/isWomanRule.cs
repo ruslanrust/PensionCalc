2a84696b98ff8d2a693d6ed85064d6498d817602
Ruslan <ruslanrust23@gmail.com>
2018-11-29 11:52:20 +0400

    Доработали

--- Gig.PensionCalc.Term/Gig.PensionCalc.Business/Rules/isWomanRule.cs
+++ Gig.PensionCalc.Term/Gig.PensionCalc.Business/Rules/isWomanRule.cs
@@ -5,11 +5,8 @@ namespace Gig.PensionCalc.Business.Rules
 {
     public class isWomanRule : ISexRule
     {
-        public Sex sex
-        {
-            get { return Sex.Woman; }
-        }
-
+        public Sex sex => Sex.Woman;
+        
         public PensionInfo Apply(PensionInfo pensionInfo, UserInfo userinfo)
         {
             pensionInfo.RemainingYears = pensionInfo.RemainingYears.Add(new TimeSpan(-365 * 2, 0, 0, 0));
